675b2bb95206c9732967b56c16a276102b0c7427
Moddassir Zaman <moddassirzaman@outlook.com>
2022-02-22 01:18:42 +0530

    first Commit

---
+++ gitFirst1/CheckPrime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitFirst1
+{
+    internal class CheckPrime
+    {
+        public void PrintPrime()
+        {
+            Console.WriteLine("prime numbers");
+        }
+    }
+}
---
+++ gitFirst1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using gitFirst1;
+
+Console.WriteLine("Hello, World!");
+CheckPrime checkPrime = new CheckPrime();
+checkPrime.PrintPrime();
